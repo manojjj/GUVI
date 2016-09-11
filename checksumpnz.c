eea3b69aa35be09bf2e71d2757db428378f24c87
manojjj <narmatha.m1996@gmail.com>
2016-09-11 13:53:47 +0530

    Create checksumpnz.c

---
+++ checksumpnz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main(void) {
+int a;
+printf("enter the value of a:");
+scanf("%d",&a);
+if(a>0)
+printf("positive");
+else if(a<0)
+printf("negative");
+else
+printf("zero");
+	return 0;
+}
